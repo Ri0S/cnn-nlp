1473103fd4cdf8a4777ae56871ffc6d0c41a85a0
Ri0S <aa248424@gmail.com>
2018-07-16 05:17:39 +0000

    a

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import utils
+import torch
 import models
+import utils
 import pickle
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from configs import config
 from tqdm import tqdm
 
-trainLoader = DataLoader(utils.Dsets(config.mode), batch_size=128, shuffle=True, collate_fn=utils.collate_fn)
-validLoader = DataLoader(utils.Dsets(config.mode), batch_size=128, shuffle=True, collate_fn=utils.collate_fn)
-
 inv_dict = pickle.load(open(config.id2char_path, 'rb'))
 config.vocab_size = len(inv_dict)
+
+trainLoader = DataLoader(utils.Dsets(config.mode), batch_size=config.batch_size, shuffle=True, collate_fn=utils.collate_fn)
+validLoader = DataLoader(utils.Dsets(config.mode), batch_size=config.batch_size, shuffle=True, collate_fn=utils.collate_fn)
+
 
 model = models.cnnNlp().to(config.device)
 
@@ -29,10 +31,11 @@ for i in range(config.n_epoch):
         total_loss += loss
         optimizer.step()
     print('epoch', i, 'loss:', total_loss.item() / idx)
-
+    torch.save(model.state_dict(), './model/epoch' + str(i))
     model.eval()
     loss = 0
     for idx, (inputs, target) in enumerate(validLoader):
         out = model(input)
         loss += criterion(out, target)
     print('valid loss: ', loss.itme() / idx)
+
