ba4477a7f3f3422c60f04e61fd06cbb942ba91f7
Ri0S <aa248424@gmail.com>
2018-07-19 08:34:57 +0000

    sep

--- models.py
+++ models.py
@@ -50,13 +50,14 @@ class ResNet(nn.Module):
             self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
             self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
             self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+            self.avgpool = nn.AvgPool2d((10, 1), stride=1)
         else:
-            self.maxpool = nn.MaxPool2d(kernel_size=(4, 1), stride=3, padding=(1, 0))
+            self.maxpool = nn.MaxPool2d(kernel_size=(3, 1), stride=2, padding=(1, 0))
             self.layer1 = self._make_layer(block, 64, layers[0], stride=2)
-            self.layer2 = self._make_layer(block, 128, layers[1], stride=3)
-            self.layer3 = self._make_layer(block, 256, layers[2], stride=3)
-            self.layer4 = self._make_layer(block, 512, layers[3], stride=3)
-        self.avgpool = nn.AvgPool2d((10, 1), stride=1)
+            self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+            self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+            self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+            self.avgpool = nn.AvgPool2d((47, 1), stride=1)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
